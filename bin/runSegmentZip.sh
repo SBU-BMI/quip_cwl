f95f70002a5620377028e1229d4e19f4d93d13c2
tkurc <tmkurc@gmail.com>
2017-03-17 11:40:38 -0400

    Created yaml descriptions of tools and workflow.

--- bin/runSegmentZip.sh
+++ bin/runSegmentZip.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-mainSegmentFeatures -t img $@ -r 1 -w 0.8 -l 3 -u 10 -k 20 -m 0.25 -j N
+echo "mainSegmentFeatures -t img $@"
+mainSegmentFeatures -t img $@ 
 
 # mainSegmentFeatures -i images/1/1.JPEG -z images/1/output.zip -o images/1 -t img -c test1 -p test1 -a seg:r1:w0.8:l3:u10:k20:jN -s 11287,13719 -b 199,192 -d 199,192 -r 1 -w 0.8 -l 3 -u 10 -k 20 -m 0.25 -j N
 
