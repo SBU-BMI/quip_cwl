9f8698c1e58dd84657c52dac078d9f7ab10b2cb4
tkurc <tmkurc@gmail.com>
2017-03-20 11:35:20 -0400

    Added codes to parse current job orders.

--- worker_cwl.js
+++ worker_cwl.js
@@ -50,3 +50,63 @@ jobs.process('quip_cwl', function(job,done) {
 	}
 });
 
+jobs.process('order', function(job,done) {
+	console.log(job.data);
+	var workflow = workflows.filter(function(el) {
+                console.log(el);
+		return el.name === "segmentation";
+	});
+        console.log(workflow);
+	if (workflow.length==0) {
+		done(new Error("Unrecognized workflow: " + job.data.name));
+	} else {
+                var job_def = {}; 
+
+                job_def.image_wsi = data.order.image.case_id;
+                job_def.locx = parseInt(data.order.roi.x);
+                job_def.locy = parseInt(data.order.roi.y);
+                job_def.width = parseInt(data.order.roi.w);
+                job_def.height = parseInt(data.order.roi.h);
+                job_def.output_dir = "./";
+                job_def.analysis_id = data.order.execution.execution_id;
+		job_def.case_id = data.order.image.case_id;
+		job_def.subject_id = data.order.image.subject_id;
+                job_def.otsu_ratio = parseFloat(data.order.execution.parameters.r);
+                job_def.curv_weight = parseFloat(data.order.execution.parameters.w);
+                job_def.lower_size = parseFloat(data.order.execution.parameters.l);
+                job_def.upper_size = parseFloat(data.order.execution.parameters.u);
+                job_def.kernel_size = parseFloat(data.order.execution.parameters.k);
+                job_def.declump = data.order.execution.parameters.j;
+		job_def.mpp = 0.25;
+                job_def.upper_left_corner = data.order.roi.x + "," + data.order.roi.y;
+                job_def.tile_size = data.order.roi.w + "," + data.order.roi.h;
+                job_def.patch_size = job_def.tile_size;
+		job_def.zip_output = "output.zip";
+                job_def.out_folder = "./temp";
+
+		var randval = Math.floor(Math.random()*100000);
+		var tmpdir  = './tmpdir'+randval;
+	        fs.mkdirSync(tmpdir);
+		var jobfile = tmpdir + '/workflow-job.json';
+		fs.writeFile(jobfile,JSON.stringify(job_def), function(err) {
+			if (err) {
+				console.log(err);
+				done(new Error("file error: " + err));
+			}
+		});
+		var cmd = 'cwltool --basedir ' + workflow[0].path + ' --outdir ' + tmpdir;
+		cmd = cmd + ' ' + workflow[0].path + '/' + workflow[0].file;
+		cmd = cmd + ' ' + jobfile;
+		console.log(cmd);
+
+		exec(cmd, function(error,stdout,stderr) {
+			if (error) {
+				console.error(error);
+				done(new Error("Execution error: " + error));
+			}
+			console.log(stdout);
+		        done(null,stdout);
+		});
+	}
+});
+
