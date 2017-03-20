8587e99c720d0bab95a4f3f7691369b5633f0a60
tkurc <tmkurc@gmail.com>
2017-03-20 08:33:39 -0400

    Added codes to retrieve image from case id.

--- bin/runCurlGet.sh
+++ bin/runCurlGet.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-curl -o image.tif -X GET http://quip-oss:5000/data/images/$1/$2,$3,$4,$5/full/0/default.tif 
+img_loc=`curl -X GET 'http://quip-data:9099/services/Camicroscope_DataLoader/DataLoader/query/getFileLocationByIID/?TCGAId=$1' | jq-unix64 '.[0]["file-location"]`;
+
+curl -o image.tif -X GET http://quip-oss:5000/$img_loc/$2,$3,$4,$5/full/0/default.tif 
 
 # /data/images/TCGA-06-0148-01Z-00-DX1.3b19c82d-c52d-4514-8bf6-5b0f629c18de.svs/11287,13719,199,192/full/0/default.jpg
 # echo "CURL" $PWD $@ 
