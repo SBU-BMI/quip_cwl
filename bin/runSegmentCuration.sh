dffa476609ea50729fefeb89af023f1046f64e96
bridge wang <tigerfan7495@gmail.com>
2017-04-13 16:34:12 -0400

    Update runSegmentCuration.sh

--- bin/runSegmentCuration.sh
+++ bin/runSegmentCuration.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-echo  "user: " $1
-echo "case_id: " $2
-
 params="user='$1',case_id='$2'"
-
-echo $params
 
 mongo quip-data:27017/quip   --eval "user='$1',case_id='$2'" /root/quip_cwl/bin/segment_curation.js > output.log
 
