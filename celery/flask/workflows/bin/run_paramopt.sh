e2cf1063ca86b73e6d532588ad2f87fe089f0f49
Erich Bremer <erich@ebremer.com>
2017-09-06 09:52:31 -0400

    WIP

--- celery/flask/workflows/bin/run_paramopt.sh
+++ celery/flask/workflows/bin/run_paramopt.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
+export HARMONY_HOME=/home/activeharmony-4.6.0/
 unzip $1
 cp /home/region-templates/build/regiontemplates/examples/Tuning-Yi/rtconf.xml .
-/home/region-templates/build/regiontemplates/examples/Tuning-Yi/sh run.sh -i ./data -f nm -d 2 -m 1 -t 0 -r 20 -x dice -u $2
+/home/region-templates/build/regiontemplates/examples/Tuning-Yi/run.sh -i ./data -f nm -d 2 -m 1 -t 0 -r 20 -x dice -u $2
 
