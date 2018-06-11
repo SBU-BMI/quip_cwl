6c97ce4331524ac580d75a9f8d7455c0cc565039
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2018-06-11 12:24:11 -0400

    added shebang to run_test.sh

--- node-kue/test/run_test.sh
+++ node-kue/test/run_test.sh
@@ -1,1 +1,2 @@
-curl -H "Content-Type: application/json" -X POST -d '{"type" : "quip_composite_cwl", "data" : { "name" : "segment_curation", "workflow" : {"user" : "tigerfan7495", "case_id" : "Alpha123"}}}'  http://quip-jobs:3000/job 
+#!/usr/bin/env bash
+curl -H "Content-Type: application/json" -X POST -d '{"type" : "quip_composite_cwl", "data" : { "name" : "segment_curation", "workflow" : {"user" : "tigerfan7495", "case_id" : "Alpha123"}}}'  http://quip-jobs:3000/job
